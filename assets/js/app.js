3ce94030f40606650128de06fb7d6790e39ce636
stephanie-rojo <stephanierojo.f@hotmail.com>
2017-12-14 16:44:29 -0300

    foodmap movil

---
+++ assets/js/app.js
@@ -0,0 +1,19 @@
+$(document).ready(function(){
+
+  $('header').fadeOut(900); //creando el splash//
+
+//  var dropdownMenu = $('#dropdownMenu1'),
+//titulo = $('.ul li ');//
+
+
+});
+
+
+//$(titulo).each(function(){
+//var li = $(this);
+//si presionamos la tecla
+//$(dropdownMenu).keyup(function(){
+//cambiamos a minusculas
+//this.value = this.value.toLowerCase();
+
+//});
